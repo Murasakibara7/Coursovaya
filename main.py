18e2097b11366f2ad25827b26e670aaf188a7679
Murasakibara Teiko <amerecana.ganjazxc@gmail.com>
2023-03-03 20:06:15 +0400

    Initial commit

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ import time
 from pprint import pprint
 from progress.bar import Bar
 
-id = '646055837'
-token_vk = 'vk1.a.HfXzgdhp_Sewd5HDyvV__Si74vkTKzME2Nk0fmkBqrA6DiH4_a9ERsgYke9dLWtDYIb2O-gA1VhenIjd1jtqfHPylhb0xx--Og4t2dgIzL5kCXNmDwfsjSCggcGGsbWs7bq8v2Sf4mxOxYa9WIouBBCEsZU747NaCFUr0ctYUS2t-ODhUUSA2xly7vLfdztr_wq0g7UjzzylTIRybY05cg'
+id = ''
+token_vk = ''
 
 
 class DownloadsPhoto:
@@ -123,4 +123,3 @@ class UploadPhoto:
 if __name__ == '__main__':
     user1 = UploadPhoto(token_yandex)
     user1.uploading_files_to_yandex_disk(user1.creating_a_new_folder_on_yandex_disk('Фотки с Vk'))
-    # user1.creating_a_new_folder_on_yandex_disk('Фотки с Vk')
